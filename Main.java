8cb27ae0be85565bc7d699e4c2818e30a789ecca
yoshida <c.yoshida.sys22@morijyobi.ac.jp>
2022-11-08 10:08:21 +0900

    ñlÌBMIðêxɌvZł«é悤ɕύX

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 public class Main {
 	public static void main (String[] args){
-		System.out.println("身長と体重を入力してください。");
-		System.out.print("身長:");
-		int height = new java.util.Scanner(System.in).nextInt();
-		System.out.print("体重:");
-		int waight = new java.util.Scanner(System.in).nextInt();
-
-		double result = waight/height^2;
+		System.out.println("二人の身長と体重を入力してください。");
 		
-		System.out.println("BMIは"+String.format("%.2f",result));
+		System.out.print("1人目身長:");
+		int height1 = new java.util.Scanner(System.in).nextInt();
+		System.out.print("1人目体重:");
+		int waight1 = new java.util.Scanner(System.in).nextInt();
+		double result1 = waight1/height1^2;
+
+		System.out.print("2人目身長:");
+                int height2 = new java.util.Scanner(System.in).nextInt();
+                System.out.print("2人目体重:");
+                int waight2 = new java.util.Scanner(System.in).nextInt();
+                double result2 = waight2/height2^2;
+		
+		System.out.println("1人目のBMIは"+String.format("%.2f",result1));
+		System.out.println("2人目のBMIは"+String.format("%.2f",result2));
 	}
 }
